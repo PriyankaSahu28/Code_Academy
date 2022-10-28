e2fab905dc0d0dcca9908f216940114743ae5df2
Priyanka Sahu <priyankasahu1428@gmail.com>
2022-10-28 14:36:29 +0530

    node js 1st commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+var mongoose = require('mongoose');
+const bodyparser=require("body-parser");
+mongoose.connect('mongodb://localhost/contactCode', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const port = 80;
+
+//define mongoose schema
+const contactSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    address: String,
+    mobile : String
+});
+
+var contact = mongoose.model('contact', contactSchema);
+
+
+
+app.use(express.static('static'))
+app.use(express.urlencoded())
+
+// PUG SPECIFIC STUFF
+app.set('view engine', 'pug') // Set the template engine as pug
+app.set('views', path.join(__dirname, 'views')) // Set the views directory
+
+
+// ENDPOINTS
+app.get('/', (req, res)=>{
+    const params = {}
+    res.status(200).render('home.pug', params);
+})
+ app.get('/contact', (req, res)=>{
+     const params = {}
+     res.status(200).render('contact.pug', params);
+ })
+
+ app.post('/contact', (req, res)=>{
+    var myData=new contact(req.body);
+    myData.save().then(()=>{
+        res.send("This item has been saved to database");
+    }).catch(()=>{
+        res.status(400).send("Item was not saved to the database ")
+    });
+
+    // res.status(200).render('contact.pug');
+})
+
+// START THE SERVER
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port ${port}`);
+});
+ 
